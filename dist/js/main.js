8a41846cb6dfc452bc30cdc06c2ebcfb1d9e364c
Flamick <adhd_ninja7@Yahoo.com>
2021-08-31 18:07:54 -0400

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,28 @@
 let itemOverlay = Array.from(document.querySelectorAll('.item'));
 
+// const handleClick = (e) => {
+//     e.preventDefault();
+//     itemOverlay.forEach(node => {
+//         node.classList.remove('clicked');
+//     });
+//     e.currentTarget.classList.add('clicked');
+// }
+
 const handleClick = (e) => {
     e.preventDefault();
-    itemOverlay.forEach(node => {
-        node.classList.remove('clicked');
-    });
-    e.currentTarget.classList.add('clicked');
+    const clicked = document.querySelector('.item.clicked');
+    const unclicked = document.querySelector('.item');
+    if(unclicked){
+        e.currentTarget.classList.add('clicked');
+        clicked.classList.remove('clicked');
+    }   else  if (clicked){
+        e.currentTarget.classList.remove('clicked');
+    }   else    {
+        clicked.classList.remove('clicked');
+    }
 }
 
 itemOverlay.forEach(node => {
     node.addEventListener('click', handleClick)
 });
+
