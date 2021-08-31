7ddd1797460809a3dbfa9df5d3aa8c98e18adaad
Flamick <adhd_ninja7@Yahoo.com>
2021-08-31 19:51:08 -0400

    js update

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,27 +1,16 @@
 let itemOverlay = Array.from(document.querySelectorAll('.item'));
 
-// const handleClick = (e) => {
-//     e.preventDefault();
-//     itemOverlay.forEach(node => {
-//         node.classList.remove('clicked');
-//     });
-//     e.currentTarget.classList.add('clicked');
-// }
-
-const handleClick = (e) => {
+const clickEffect = (e) => {
     e.preventDefault();
-    const clicked = document.querySelector('.item.clicked');
+    let clicked = document.querySelector('.item.clicked');
     const unclicked = document.querySelectorAll('.item');
     if(unclicked){
         e.currentTarget.classList.add('clicked');
         clicked.classList.remove('clicked');
-    }   else  if (clicked){
-        e.currentTarget.classList.remove('clicked');
     }
-    
 }
 
 itemOverlay.forEach(node => {
-    node.addEventListener('click', handleClick)
+    node.addEventListener('click', clickEffect)
 });
 
